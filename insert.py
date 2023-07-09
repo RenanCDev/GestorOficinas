61eb5ebe14ccc1da2bdd813ce898f533fb020e39
RenanMRb <133038706+RenanMRb@users.noreply.github.com>
2023-07-08 23:35:20 -0300

    Projeto1.4

--- insert.py
+++ insert.py
@@ -1,5 +1,29 @@
-def option_pesq():
-    print("\t1 - Pesquisar por CPF e identificador")
-    print("\t0 - Coltar ao menu anterior")
-    print()
-    option = input("O que deseja fazer ? ")
+            id = insert_id()
+            if id in orcamentos:
+                print_orcament_0(id)
+                option = option_orcament()
+                while option != "0" and option != "1" and option != "2" and option != "3":
+                    data_invalid()
+                    print_orcament_0(id)
+                    option = option_orcament()
+                if option == "1":
+                    os.system("clear")
+                    print("Orçamento transformado em ordem de serviço aberta com sucesso!")
+                    print()
+                    input("Tecle ENTER para continuar")
+                    transf_ord_serv_abert(id)
+                elif option == "2":
+                    orcamentos[id] = edit_orcament(id)
+                elif option == "3":
+                    del(orcamentos[id])
+                    os.system("clear")
+                    print("Orçamento excluido com sucesso!")
+                    print()
+                    input("Tecle ENTER para continuar")
+                else:
+                    data_invalid()
+            else:
+                os.system("clear")
+                print("IDENTIFICADOR NÃO CADASTRADO!!!")
+                print()
+                input("Tecle ENTER para continuar")
